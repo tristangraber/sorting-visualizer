0df009614e4d3fa92c95a30d645248d2f3fceaa2
Ethan Wert <ethan.wert01@gmail.com>
2021-04-21 03:22:18 -0400

    selection sort (almost) completed

--- src/SortingAlgorithms/SortingAlgorithms.js
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -3,21 +3,3 @@ Sorting Algorithm Visualizer JS file for STEM EE 2nd-Year Capstone project
 Names: Tristan Graber and Ethan Wert
 Date: 4/21/21
 */
-
-function bubbleSort(array){
-    const animations = [];
-    if (array.length == 1) return array;
-
-    let size = array.length;
-    for (let i = 0; i < size - 1; i++) {
-        for (let j = 0; j < size-i-1; j++) {
-            animations.push(j);
-            if(array[j] > array[j+1]) {
-                let temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-    return animations;
-};
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,7 +1,7 @@
 /*
-Sorting Algorithm Visualizer JSX file for STEM EE 2nd-Year Capstone project
-Names: Tristan Graber and Ethan Wert
-Date: 4/21/21
+    Sorting Algorithm Visualizer JSX file for STEM EE 2nd-Year Capstone project
+    Names: Tristan Graber and Ethan Wert
+    Date: 4/21/21
 */
 
 import React from 'react';
@@ -11,7 +11,8 @@ import './SortingVisualizer.css';
 const PRIMARY_COLOR = 'lightblue';
 const SECONDARY_COLOR = 'orange';
 const FINAL_COLOR = 'lightgreen';
-const ANIMATION_SPEED = 3;
+const ANIMATION_SPEED = 1;
+const ARRAY_SIZE = 100;
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -20,21 +21,28 @@ export default class SortingVisualizer extends React.Component {
         this.state = { array: [] };
     }
 
-    // runs at start to generate a new array
+    /*
+        Runs at the start to generate a new random array for display.
+    */
     componentDidMount() {
         this.generateNewArray();
     }
 
-    // generates a new random array for display
+    /*
+        Generates a new random array for display.
+    */
     generateNewArray() {
         const array = [];
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < ARRAY_SIZE; i++) {
             array.push(randomInt(5,520));
         }
         this.setState({array});
         this.resetColor();
     }
 
+    /*
+        Resets all bars color to primary color.
+    */
     resetColor() {
         const arrayBars = document.getElementsByClassName('array-bar');
         for (let i = 0; i < arrayBars.length; i++) {
@@ -50,37 +58,38 @@ export default class SortingVisualizer extends React.Component {
         const animations = getBubbleSortAnimations(this.state.array);
         // gets array of all the array-bar objects that are currently in the DOM
         const arrayBars = document.getElementsByClassName('array-bar');
+        // loop for each value of the animation (pt 1: primary to secondary, pt 2: secondary to other)
         for (let i = 0; i < animations.length; i++) {
             // get first bar and next bar
             const barOneIdx = animations[i];
             const barTwoIdx = animations[i] + 1;
-            // applies style from render to set heights
+            // create variables to change height
             const barOneStyle = arrayBars[barOneIdx].style;
             const barTwoStyle = arrayBars[barTwoIdx].style;
             let color = PRIMARY_COLOR;
             // delays the action so it doesn't happen instantly
             setTimeout(() => {
-                // if it is a blue to orange color change
+                // if it is a primary to secondary color change
                 if (i % 2 === 0) {
                     color = SECONDARY_COLOR;
-                    // save the heights
+                    // screate variables to store height
                     let barOneHeight = barOneStyle.height;
                     let barTwoHeight = barTwoStyle.height;
                     // swap if the height of first is greater than second
-                    if (parseInt(barOneHeight, 10) > parseInt(barTwoHeight,10)) {    
+                    if (parseInt(barOneHeight, 10) > parseInt(barTwoHeight, 10)) {
                         barOneStyle.height = barTwoHeight;
                         barTwoStyle.height = barOneHeight;
                     }
                 }
-                // set the colors
+                // set the colors (secondary if first part of ani, primary if second part of ani)
                 barOneStyle.backgroundColor = color;
                 barTwoStyle.backgroundColor = color;
-                // if it is at the end
-                if (animations[i] > animations[i+1]) {
+                // if it is at the end, change to final color
+                if (animations[i] > animations[i + 1]) {
                     arrayBars[animations[i] + 1].style.backgroundColor = FINAL_COLOR;
                 }
-                // 
-                if (animations.length - i < 3) { /* to color in last two bars */
+                // color first two bars green when finished
+                if (animations.length - i < 3) {
                     arrayBars[animations[i]].style.backgroundColor = FINAL_COLOR;
                     arrayBars[animations[i] + 1].style.backgroundColor = FINAL_COLOR;
                 }
@@ -88,8 +97,53 @@ export default class SortingVisualizer extends React.Component {
         }
     }
 
+    /*
+        Runs the Selection Sort sorting algorithm on the current array.
+    */
     selectionSort() {
-
+        const oldArray = Array.from(this.state.array);
+        // get array of animations for selection sort
+        const animations = getSelectionSortAnimations(this.state.array);
+        // loop for each value of the animation (pt 1: primary to secondary, pt 2: secondary to other)
+        for (let i = 0; i < animations.length; i = i + 2) {
+            // gets array of all the array-bar objects that are currently in the DOM
+            const arrayBars = document.getElementsByClassName('array-bar');
+            // get first bar and next bar
+            const barOneIdx = animations[i];
+            const barTwoIdx = animations[i + 1];
+            // create variables to change height
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            let color = PRIMARY_COLOR;
+            // delays the action so it doesn't happen instantly
+            setTimeout(() => {
+                // if it is a primary to secondary color change
+                if (i % 4 === 0) {
+                    color = SECONDARY_COLOR;
+                    barOneStyle.backgroundColor = color;
+                }
+                // set the colors (secondary if first part of ani, primary if second part of ani)
+                barTwoStyle.backgroundColor = color;
+                // if it is at the end, change to final color
+                if (animations[i + 1] > animations[i + 3]) {
+                    // find the index of the next sorted element
+                    let idx = findElementIdx(this.state.array[barOneIdx], arrayBars);
+                    // determine heights
+                    let minBarStyle = arrayBars[idx].style;
+                    let minBarHeight = minBarStyle.height;
+                    let swapBarHeight = barOneStyle.height;
+                    // swap heights and change color to final color
+                    barOneStyle.height = minBarHeight;
+                    minBarStyle.height = swapBarHeight;
+                    arrayBars[barOneIdx].style.backgroundColor = FINAL_COLOR;
+                }
+                // color first two bars green when finished
+                if (animations.length - i < 3) {
+                    arrayBars[animations[i]].style.backgroundColor = FINAL_COLOR;
+                    arrayBars[animations[i] + 1].style.backgroundColor = FINAL_COLOR;
+                }
+            }, i * ANIMATION_SPEED);
+        }
     }
 
     render() {
@@ -106,17 +160,23 @@ export default class SortingVisualizer extends React.Component {
             }}></div>
         ))}
             <button className="button" onClick={() => this.generateNewArray()}>Generate New Array</button>
-            <button className="button" onClick={() => this.selectionSort()}>Selection Sort</button>
             <button className="button" onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            <button className="button" onClick={() => this.selectionSort()}>Selection Sort</button>
           </div>
         );
       }
     }
 
+    /*
+        Generates a random integer between a given min and max.
+    */
     function randomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
+    /*
+        Determines if two given arrays are equal.
+    */
     function arraysAreEqual(array1, array2) {
         if (array1.length !== array2.length) return false;
         for (let i = 0; i < array1.length; i++) {
@@ -128,7 +188,7 @@ export default class SortingVisualizer extends React.Component {
     /*
         Returns an array of animations for the Bubble Sort algorithm.
     */
-    function getBubbleSortAnimations(array){
+    function getBubbleSortAnimations(array) {
         const animations = [];
         if (array.length === 1) return array;
     
@@ -149,5 +209,47 @@ export default class SortingVisualizer extends React.Component {
             }
         }
         return animations;
-    };
+    }
 
+    /*
+        Returns an array of animations for the Selection Sort algorithm.
+    */
+   function getSelectionSortAnimations(array) {
+        const animations = [];
+        if (array.length === 1) return array;
+
+        let size = array.length;
+        // # of sorting runs through the array
+        for (let i = 0; i < size - 1; i++) {
+            let min = i;
+            // a single sorting run through the array
+            for (let j = 1 + i; j < size; j++) {
+                animations.push(i);
+                animations.push(j);
+                animations.push(i);
+                animations.push(j);
+                // find min
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            // swap min for start index
+            let temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+        return animations;
+   }
+
+   /*
+        Finds a given element in a given array.
+   */
+  function findElementIdx(element, array) {
+        let result = -1;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].style.height === element + 'px') {
+                result = i;
+            }
+        }
+        return result;
+  }
